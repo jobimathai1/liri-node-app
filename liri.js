61dbb576181334f710dd95e79fc8bf85346fc8b3
Jobi Mathai <jobi.mathai@gmail.com>
2018-10-27 16:28:29 -0700

    added loop to go through command & switch statement

--- liri.js
+++ liri.js
@@ -5,7 +5,42 @@ var spotify = new Spotify(keys.spotify);
 var request = require("request");
 var fs = require("fs");
 
+//Store the node command line arguments
+var cmdArgs = process.argv;
+var additionalArg = "";
+//Liri command comes after "node filename.js"
+var liriCmdArgs = cmdArgs[2];
+
+//In case of space in the argument
+for (var i = 3; i < cmdArgs.length; i++) {
+    if (i > 3 && i < cmdArgs.length) {
+        additionalArg = additionalArg + "+"
+        cmdArgs[i];
+    } else {
+        additionalArg = additionalArg + cmdArgs[i];
+    }
+}
+
+
 //Create Switch Case statements for Spotify, OMDB, Bands In Town
+
+switch (liriCmdArgs) {
+    case 'spotify-this-song':
+        spotify();
+        break;
+
+    case 'movie-this':
+        imdb();
+        break;
+
+    case 'movie-this':
+        imdb();
+        break;
+
+    case 'do-what-it-says':
+        doWhat();
+        break;
+}
 
 //Write a function to search through Spotify for songs based on user input, if song not found then else to Ace of Base
 
