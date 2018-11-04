6665a444ba5d3b88bf44b6098e4d42b3d91e445f
Jobi Mathai <jobi.mathai@gmail.com>
2018-11-03 18:04:41 -0700

    spotify functionality added

--- liri.js
+++ liri.js
@@ -1,53 +1,43 @@
-require("dotenv").config();
-var keys = require('./keys');
-
-var spotify = new Spotify(keys.spotify);
-var request = require("request");
+var Spotify = require('node-spotify-api');
+require('dotenv').config();
 var fs = require("fs");
+var keys = require('./keys.js');
 
-//Store the node command line arguments
-var cmdArgs = process.argv;
-var additionalArg = "";
-//Liri command comes after "node filename.js"
-var liriCmdArgs = cmdArgs[2];
+var nodeArgs = process.argv;
+var appArgs = process.argv[2];
 
-//In case of space in the argument
-for (var i = 3; i < cmdArgs.length; i++) {
-    if (i > 3 && i < cmdArgs.length) {
-        additionalArg = additionalArg + "+"
-        cmdArgs[i];
+var input = "";
+
+for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        input += " " + nodeArgs[i];
     } else {
-        additionalArg = additionalArg + cmdArgs[i];
+        input += nodeArgs[i];
     }
 }
 
-
-//Create Switch Case statements for Spotify, OMDB, Bands In Town
-
-switch (liriCmdArgs) {
-    case 'spotify-this-song':
-        spotify();
-        break;
-
-    case 'movie-this':
-        imdb();
-        break;
-
-    case 'movie-this':
-        imdb();
-        break;
-
-    case 'do-what-it-says':
-        doWhat();
-        break;
+switch(appArgs) {
+    case 'spotify-this-song': getSong(input);
+    break;
 }
 
-//Write a function to search through Spotify for songs based on user input, if song not found then else to Ace of Base
+//Function for Spotify
+function getSong(songName) {
+    var spotify = new Spotify(keys.spotify);
 
-//Write a function to search through OMDB for movie based on user input
+    //If no song is provided, use "The Sign" 
+        if (!songName) {
+            songName = "The Sign";
+        };        
 
-//Write a function to search through Bands in Town for concerts based on user input
+        console.log(songName);
 
-//Write a function to go through random.txt file and do what it says
-
-//Instead of just logging to console, look into logging into a log.txt file
+        //Callback to spotify to search for song name
+        spotify.search({ type: 'track', query: songName}, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            } 
+            console.log("Artist: " + data.tracks.items[0].artists[0].name + "\nSong name: " + data.tracks.items[0].name +
+            "\nAlbum Name: " + data.tracks.items[0].album.name + "\nPreview Link: " + data.tracks.items[0].preview_url); 
+        });
+};
